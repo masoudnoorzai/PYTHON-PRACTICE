f609ceff98e6aa7c00597bb5be8cf452b056a337
Masoud Norouzi <masoudn071@gmail.com>
2026-07-12 12:54:55 +0330

    add student grade program

---
+++ student_grade.py
@@ -0,0 +1,11 @@
+score = int(input("نمره خود را وارد نمایید: "))
+if score >=18 and score <= 20:
+    print("عالی است")
+elif score >= 15 and score <=17 :
+    print("خیلی خوب است ")
+elif score>=12 and score <= 14 :
+    print("قبول ")
+else :
+    print("مردود")
+
+
