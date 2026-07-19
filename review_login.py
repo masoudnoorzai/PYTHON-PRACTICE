010906c38875659b4c69ed0fbf0b9910495f2afc
Masoud Norouzi <masoudn071@gmail.com>
2026-07-19 21:45:07 +0330

    Review and practice condition

---
+++ review_login.py
@@ -0,0 +1,8 @@
+age = int(input("سن خود را وارد کنید"))
+has_card = input("ایا کارت ملی دارید yes or no : ")
+if age >= 18 and has_card == "yes" :
+    print("ورود مجاز است")
+else :
+    print("ورود مجاز نیست")
+    
+
